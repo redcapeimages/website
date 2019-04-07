41acf4d74e8d5476edef4b9fe11f0dde2132d4a9
redcapeimages <49354691+redcapeimages@users.noreply.github.com>
2019-04-06 21:35:43 -0400

    Add files via upload

---
+++ precache-manifest.bffb4bf5e6d019e20548a417544e3adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a651c5b11461bef50f54725df1a7126b",
+    "url": "/static/media/menu.a651c5b1.png"
+  },
+  {
+    "revision": "cfd435343b6f9275d88286858a465ec5",
+    "url": "/static/media/logo.cfd43534.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7513eb7f3c84c922a83d",
+    "url": "/static/js/main.ef3d86f0.chunk.js"
+  },
+  {
+    "revision": "401e6cd6d0bea889b862",
+    "url": "/static/js/2.03b53e62.chunk.js"
+  },
+  {
+    "revision": "7513eb7f3c84c922a83d",
+    "url": "/static/css/main.76517eba.chunk.css"
+  },
+  {
+    "revision": "401e6cd6d0bea889b862",
+    "url": "/static/css/2.ffc3df7b.chunk.css"
+  },
+  {
+    "revision": "a6245d80831607241009de93100a9fa7",
+    "url": "/index.html"
+  }
+];
